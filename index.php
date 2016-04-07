02a9d3afb75c8262b16f6aa3e0fd4e6df6383910
jobin mathew <jobinvcm@gmail.com>
2016-04-07 12:08:06 +1000

    initialiaing allfile

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+include('simple_html_dom.php');
+
+$url = "http://www.ihna.edu.au";
+$html=file_get_html($url);
+$imglinks=array();
+$imglinksref=array();
+
+foreach($html->find(' div div div  a img') as $element) 
+       array_push($imglinks,$element->src);
+
+foreach($imglinks as $links){
+    if(strpos($links,'adv')){
+        array_push($imglinksref,$links);
+    }
+}
+
+
+print_r($imglinksref);
+
+echo '<img src="' . $imglinksref[0] . '" width="700" height="200" style="vertical-align:middle"/>';
+echo '<img src="' . $imglinksref[1] . '" width="700" height="200" style="vertical-align:middle"/>';
+echo '<img src="' . $imglinksref[2] . '" width="700" height="200" style="vertical-align:middle"/>';
+echo '<img src="' . $imglinksref[3] . '" width="700" height="200" style="vertical-align:middle"/>';
+echo '<img src="' . $imglinksref[4] . '" width="700" height="200" style="vertical-align:middle"/>';
+?>
